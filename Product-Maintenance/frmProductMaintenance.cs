aa73d1fe402302219f4cacf6c931073ac0627ba6
Blake McLeod - TAFE <b-mcleod@outlook.com>
2014-08-21 14:49:36 +1000

    First Commit
    Solution is correctly setup and database file setup so it will not be
    copied into each project within the solution.
    
    Assignment 1 is completed.
    Assignment 2 has been started.

---
+++ Product-Maintenance/frmProductMaintenance.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Product_Maintenance
+{
+	public partial class frmProductMaintenance : Form
+	{
+		/* Default Initializer */
+
+		public frmProductMaintenance()
+		{
+			InitializeComponent();
+		}
+
+		/* Save Button */
+
+		private void productsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+		{
+			this.Validate();
+			try
+			{
+				this.productsBindingSource.EndEdit();
+				this.tableAdapterManager.UpdateAll(this.techSupportDataSet);
+			}
+			catch (DBConcurrencyException)
+			{
+				MessageBox.Show("A concurrency error occurred. " +
+					"Some rows were not updated. ", "Save Error");
+				this.productsTableAdapter.Fill(this.techSupportDataSet.Products);
+			}
+			catch (SqlException ex)
+			{
+				MessageBox.Show(
+					"Database error # " + ex.Number +
+					": " + ex.Message, ex.GetType().ToString());
+			}
+		}
+
+		/* Form Load */
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			// TODO: This line of code loads data into the 'techSupportDataSet.Products' table. You can move, or remove it, as needed.
+			try
+			{
+				this.productsTableAdapter.Fill(this.techSupportDataSet.Products);
+			}
+			catch (SqlException ex)
+			{
+				MessageBox.Show("Database error # " + ex.Number +
+					": " + ex.Message, ex.GetType().ToString());
+			}
+		}
+
+		/* Data Error Method */
+
+		private void productsDataGridView_DataError(object sender, DataGridViewDataErrorEventArgs e)
+		{
+			int row = e.RowIndex + 1;
+			string errorMessage = "A data error occurred.\n" +
+				"Row: " + row + "\n" +
+				"Error: " + e.Exception.Message;
+			MessageBox.Show(errorMessage, "Data Error");
+		}
+	}
+}
