c69c8a1334d6c68c750ed051e57c5937f4b16105
Blake McLeod - TAFE <b-mcleod@outlook.com>
2014-09-18 14:43:21 +1000

    Term Finished
    Final Commit for Term 3

--- Register-Products/Properties/Settings.Designer.cs
+++ Register-Products/Properties/Settings.Designer.cs
@@ -13,14 +13,5 @@ namespace Register_Products.Properties {
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
-        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
-                return defaultInstance;
-            }
-        }
-    }
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase { }
 }
